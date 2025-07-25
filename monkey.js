09744a713bdb8ac6e3a9ece5a22e4f94e702dd74
jajet.loh <jajetloh@gmail.com>
2025-07-26 07:35:42 +0800

    fix monkey.js

--- monkey.js
+++ monkey.js
@@ -1,10 +1,1 @@
-javascript:(()=>{  
-    fetch('https://raw.githubusercontent.com/jajetloh/ic-combo-logger/main/script.js?cachebust='+Date.now())  
-      .then(r=>r.text())  
-      .then(code=>{  
-        // Running this eval as "user code" bypasses CSP
-        /* eslint-disable no-eval */  
-        eval(code);  
-      })  
-      .catch(console.error);  
-  })();
+javascript:(()=>{fetch('https://raw.githubusercontent.com/jajetloh/ic-combo-logger/main/script.js?cachebust='+Date.now()).then(r=>r.text()).then(code=>{eval(code);}).catch(console.error);})();
